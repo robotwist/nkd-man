9e98f54d48245b219cf0be22074d75016f6371e3
Rob Wistrand <robwistrand@gmail.com>
2025-02-16 14:43:24 -0800

    cop assets and animation

--- nkdman.js
+++ nkdman.js
@@ -1,8 +1,8 @@
-// NK D Man - The Ultimate Viral Streaker
+// NK D Man - The Ultimate Viral Streaker with Tiny Cop Chasers
 (function() {
     // Create the sprite element
     let nkdMan = document.createElement("img");
-    nkdMan.src = "https://your-hosted-image.com/nkdman_frame_0.png"; // Placeholder URL
+    nkdMan.src = "https://robotwist.github.io/nkd-man/assets/nkdman_frame_0.png";
     nkdMan.style.position = "fixed";
     nkdMan.style.left = "-100px";
     nkdMan.style.bottom = Math.random() * window.innerHeight * 0.6 + "px";
@@ -14,11 +14,10 @@
     let position = -100;
     let speed = 10;
     let frame = 0;
-   let frameImages = [
-    "https://robotwist.github.io/nkd-man/assets/nkdman_frame_0.png",
-    "https://robotwist.github.io/nkd-man/assets/nkdman_frame_1.png"
-];
-
+    let frameImages = [
+        "https://robotwist.github.io/nkd-man/assets/nkdman_frame_0.png",
+        "https://robotwist.github.io/nkd-man/assets/nkdman_frame_1.png"
+    ];
 
     function animate() {
         position += speed;
@@ -30,7 +29,36 @@
             requestAnimationFrame(animate);
         } else {
             document.body.removeChild(nkdMan);
+            setTimeout(spawnCopChasers, 1000); // Delay cops appearing
             setTimeout(returnForEncore, Math.random() * 5000 + 5000); // Delayed encore
+        }
+    }
+
+    function spawnCopChasers() {
+        let copCount = Math.floor(Math.random() * 3) + 1; // 1 to 3 cops
+        for (let i = 0; i < copCount; i++) {
+            let cop = document.createElement("img");
+            cop.src = "https://robotwist.github.io/nkd-man/assets/cop_man.webp";
+            cop.style.position = "fixed";
+            cop.style.left = "-80px";
+            cop.style.bottom = Math.random() * window.innerHeight * 0.6 + "px";
+            cop.style.width = "40px";
+            cop.style.zIndex = "999999";
+            document.body.appendChild(cop);
+            
+            let copPosition = -80;
+            let copSpeed = 8;
+            function animateCop() {
+                copPosition += copSpeed;
+                cop.style.transform = `translateX(${copPosition}px)`;
+                
+                if (copPosition < window.innerWidth) {
+                    requestAnimationFrame(animateCop);
+                } else {
+                    document.body.removeChild(cop);
+                }
+            }
+            animateCop();
         }
     }
 
@@ -60,7 +88,7 @@
         }
         
         setTimeout(() => {
-            nkdManEncore.src = "https://your-hosted-image.com/nkdman_wave.png";
+            nkdManEncore.src = "https://robotwist.github.io/nkd-man/assets/nkdman_wave.png";
         }, 1000); // Midway wave before exit
         
         animateEncore();
