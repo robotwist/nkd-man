75d3870ab00b58bc81541305c85cae678029f036
Rob Wistrand <robwistrand@gmail.com>
2025-02-16 19:57:46 -0800

    restricted NKD

--- background.js
+++ background.js
@@ -1,6 +1,11 @@
 chrome.action.onClicked.addListener((tab) => {
+    if (tab.url.startsWith("chrome://") || tab.url.startsWith("about:")) {
+        console.log("NK D Man cannot streak on Chrome settings pages.");
+        return; // Stop execution
+    }
+
     chrome.scripting.executeScript({
         target: {tabId: tab.id},
         files: ["content.js"]
-    });
+    }).catch(err => console.error("Error executing script:", err));
 });
