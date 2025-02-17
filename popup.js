817b149e7038f1e80b1732b740918f86df9fc602
Rob Wistrand <robwistrand@gmail.com>
2025-02-16 19:46:20 -0800

    chrome-tabs instead of chrome-scripting caused err

--- popup.js
+++ popup.js
@@ -1,8 +1,12 @@
 document.getElementById("streak").addEventListener("click", () => {
     chrome.tabs.query({active: true, currentWindow: true}, (tabs) => {
-        chrome.scripting.executeScript({
-            target: {tabId: tabs[0].id},
-            files: ["content.js"]
-        });
+        if (chrome.scripting) {
+            chrome.scripting.executeScript({
+                target: {tabId: tabs[0].id},
+                files: ["content.js"]
+            });
+        } else {
+            chrome.tabs.executeScript(tabs[0].id, {file: "content.js"});
+        }
     });
 });
