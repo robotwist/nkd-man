e8d04bb825dd9958fef0f6faf92113c2b39feda6
Rob Wistrand <robwistrand@gmail.com>
2025-02-16 16:10:57 -0800

    simple nkd man

--- nkdman.js
+++ nkdman.js
@@ -1,4 +1,4 @@
-// NK D Man - The Ultimate Viral Streaker with Tiny Cop Chasers
+// NK D Man - The Ultimate Viral Streaker (Simplified Version)
 (function() {
     // Create the sprite element
     let nkdMan = document.createElement("img");
@@ -11,7 +11,7 @@
     document.body.appendChild(nkdMan);
 
     let position = -100;
-    let speed = 12;
+    let speed = 9;
     let frame = 0;
     let frameImages = [
         "https://robotwist.github.io/nkd-man/assets/nkdman_frame_0.png",
@@ -34,38 +34,7 @@
             requestAnimationFrame(animate);
         } else {
             document.body.removeChild(nkdMan);
-            setTimeout(spawnCopChasers, 1000); // Delay cops appearing
             setTimeout(returnForEncore, Math.random() * 5000 + 5000); // Delayed encore
-        }
-    }
-
-    function spawnCopChasers() {
-        let copCount = Math.floor(Math.random() * 3) + 1; // 1 to 3 cops
-        for (let i = 0; i < copCount; i++) {
-            let cop = document.createElement("img");
-            cop.src = "https://robotwist.github.io/nkd-man/assets/cop_man.webp";
-            cop.style.position = "fixed";
-            cop.style.left = "-80px";
-            cop.style.bottom = Math.random() * window.innerHeight * 0.6 + "px";
-            cop.style.width = "40px";
-            cop.style.zIndex = "999999";
-            document.body.appendChild(cop);
-            
-            let copPosition = -80;
-            let copSpeed = 9;
-            let tripAt = Math.random() * window.innerWidth * 0.5 + window.innerWidth * 0.2;
-            function animateCop() {
-                copPosition += copSpeed;
-                cop.style.transform = `translateX(${copPosition}px)`;
-                
-                if (copPosition < tripAt) {
-                    requestAnimationFrame(animateCop);
-                } else {
-                    cop.src = "https://robotwist.github.io/nkd-man/assets/cop_trip.webp"; // Tripping frame
-                    setTimeout(() => document.body.removeChild(cop), 1000);
-                }
-            }
-            animateCop();
         }
     }
 
@@ -95,7 +64,7 @@
                 requestAnimationFrame(animateEncore);
             } else {
                 setTimeout(() => {
-                    nkdManEncore.src = "https://robotwist.github.io/nkd-man/assets/nkdman_rockon.png";
+                    nkdManEncore.src = "https://robotwist.github.io/nkd-man/assets/nkdman_frame_1.png";
                 }, 500);
                 setTimeout(() => {
                     document.body.removeChild(nkdManEncore);
